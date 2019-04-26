ab3a9a3e583fb660576b96fdd247f12b93b99e9f
Matt Fara <mfara@burstingsilver.com>
2019-04-26 15:06:59 -0400

    Service and ViewModel stuff

--- MinionMaster/MinionMaster/MinionMaster/Services/DirectionService.cs
+++ MinionMaster/MinionMaster/MinionMaster/Services/DirectionService.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Reactive;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
 namespace MinionMaster.Services
 {
-    public class DirectionService
+    public class DirectionService : IDirectionService
     {
+        readonly Subject<Unit> partnerStatusChangedSubject;
+        readonly Subject<Unit> gpsReceivedSubject;
+        readonly Subject<double> compassDirectionReceivedSubject;
+
+        public IObservable<Unit> PartnerStatusChanged() => this.partnerStatusChangedSubject;
+        public IObservable<Unit> GpsReceived() => this.gpsReceivedSubject;
+        public IObservable<double> CompassDirectionReceived() => this.compassDirectionReceivedSubject;
+
         public DirectionService()
         {
+            this.compassDirectionReceivedSubject = new Subject<double>();
+            this.gpsReceivedSubject = new Subject<Unit>();
+            this.partnerStatusChangedSubject = new Subject<Unit>();
+        }
+
+        public Task BrandMe(string brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendDirection(double heading)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendGps(double latitude, double longitude)
+        {
+            throw new NotImplementedException();
         }
     }
 }
